ba513e274022b88993acdf0601c6587d52de3bcb
Cano <159212786+CaDCano@users.noreply.github.com>
2025-12-03 21:30:12 -0500

    Add Boolean import to models.py

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime, Boolean
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from database import Base
